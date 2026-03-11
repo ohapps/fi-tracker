27c7fe26154b0d57f66a8e5760cacabdfffaa5f1
Craig Hausner <craig_hausner@unigroup.com>
2026-03-10 21:36:01 -0500

    updates based on MR feedback

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,6 +3,8 @@ import * as Sentry from '@sentry/nextjs';
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
+  integrations: [Sentry.replayIntegration()],
+
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
